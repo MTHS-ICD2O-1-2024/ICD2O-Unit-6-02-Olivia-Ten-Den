f665bafb1687852aa36bab33fe28d237954c49f0
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-20 16:46:50 +0000

    1

--- js/script.js
+++ js/script.js
@@ -1,51 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Olivia Ten Den
-// Created on: May 16, 2025
+// Created on: May 15, 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
 window.onload = function () {
-	// Retrieve firstInteger from localStorage
-	let storedInteger = localStorage.getItem('firstInteger');
-
-	// Get the integer-one input element
-	const firstInteger = document.getElementById('integer-one');
-
-	// Set the integer-one input value if storedValue exists in localStorage
-	if (storedInteger) {
-	  firstInteger.value = storedInteger;
-	}
-
-	// Retrieve secondInteger from localStorage
-	storedInteger = localStorage.getItem('secondInteger');
-
-	// Get the integer-one input element
-	const secondInteger = document.getElementById('integer-two');
-
-	// Set the integer-one input value if storedValue exists in localStorage
-	if (storedInteger) {
-	  secondInteger.value = storedInteger;
-	}
+  if (localStorage.hits) {
+    displayCount()
+  }
+  else {
+    localStorage.setItem('hits', 0)
+    displayCount()
+  }
 }
 
-function calculate() {
-  let counter = 0
-  let firstInteger = parseInt(document.getElementById("integer-one").value)
-  localStorage.setItem('firstInteger', firstInteger)
-  let answer = firstInteger
-  let secondInteger = parseInt(document.getElementById("integer-two").value)
-  localStorage.setItem('secondInteger', secondInteger)
-  
-  while (true) {
-    if (answer < secondInteger) {
-      break
-    }
-    else {
-      answer = answer - secondInteger
-      counter = counter + 1
-    }
-  }
-  document.getElementById('result').innerHTML = "<p>The quotient is " + counter + ", remainder " + answer + ".</p>"
+function clickCookie() {
+  localStorage.hits = Number(localStorage.hits) + 1
+  displayCount()
+}
+
+function displayCount() {
+  document.getElementById('result').innerHTML = "<h5>Total Hits : " + localStorage.hits + "</h5>"
 }
